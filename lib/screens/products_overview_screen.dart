076f488cbe941bfa287401832de37579cbb9b3ae
Luiz Felipe Sampaio Castelo Branco Moreira <luiz-felipe.moreira@serpro.gov.br>
2020-03-04 15:34:45 -0300

    working on the shopping cart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_udemy_shop_app/screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
@@ -57,7 +58,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
               icon: Icon(
                 Icons.shopping_cart,
               ),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
             ),
           ),
         ],
