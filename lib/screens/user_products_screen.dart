6c4f2ae6741369766499a93bd662d46709b34fae
Luiz Felipe Moreira <felipecbm@gmail.com>
2020-03-22 12:13:05 -0300

    saving, updating and deleting products

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_udemy_shop_app/screens/edit_product_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
 import '../widgets/user_product_item.dart';
 import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
@@ -20,7 +20,6 @@ class UserProductsScreen extends StatelessWidget {
             icon: const Icon(Icons.add),
             onPressed: () {
               Navigator.of(context).pushNamed(EditProductScreen.routeName);
-              // ...
             },
           ),
         ],
@@ -33,6 +32,7 @@ class UserProductsScreen extends StatelessWidget {
           itemBuilder: (_, i) => Column(
                 children: [
                   UserProductItem(
+                    productsData.items[i].id,
                     productsData.items[i].title,
                     productsData.items[i].imageUrl,
                   ),
